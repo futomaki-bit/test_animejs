cad6884e5e2431720f1035683feb3bf6b195cb6d
futomakiyoin <futomakiyoin@gmail.com>
2021-06-15 14:31:20 -0400

    Throw animation

--- js/animate.js
+++ js/animate.js
@@ -1,20 +1,10 @@
-// Scatter as if someone threw an object and landed somewhere random
-// it will move in x and y axes and rotate to make it more realistic
-let scatter = anime.set('.scattered', {
-    translateX: function () {
-        return anime.random(-200, 200);
-    },
-    translateY: function () {
-        return anime.random(-200, 200);
-    },
-    rotate: function () {
-        return anime.random(-90, 90);
-    },
-});
-
 const SLIDEDURATION = 1500;
 const OFFSET = 1000
 const FADEIN = [0, 1]
+
+// Scatter as if someone threw an object and landed somewhere random
+// it will move in x and y axes and rotate to make it more realistic
+
 
 anime.timeline().add({
     // transition from -1000 (left) to 0 (center)
@@ -36,7 +26,23 @@ anime.timeline().add({
     // fade in other text
     targets: '.other',
     opacity: FADEIN,
+    translateY: [1000, 0],
     easing: "easeOutExpo",
     duration: 2 * SLIDEDURATION,
     delay: 0
+}).add({
+    targets: '.scattered',
+    translateX: function () {
+        return anime.random(-200, 200);
+    },
+    translateY: function () {
+        return anime.random(-200, 200);
+    },
+    rotate: function () {
+        return anime.random(-90, 90);
+    },
+    opacity: [0, 0.5],
+    fontSize: [100, 20],
+    duration: SLIDEDURATION,
+
 });
